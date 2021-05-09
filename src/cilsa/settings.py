acee7e4547595e5af464b81c5973456ef4bb784d
frank <frnk.doze@gmail.com>
2021-05-08 23:58:37 -0500

    Second commit

--- src/cilsa/settings.py
+++ src/cilsa/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #own
+    'pages',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +58,7 @@ ROOT_URLCONF = 'cilsa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [Path.joinpath(BASE_DIR,"templates")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ src/pages/views.py
@@ -0,0 +1,24 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def home_view(request,*args,**kwargs):
+    print(args,kwargs)
+    print(request.user)
+    return render(request, "home.html", {})
+
+def contact_view(request,*args,**kwargs):
+    return render(request, "contact.html", {})
+
+def about_view(request,*args,**kwargs):
+    my_context = {
+        "title" : "this is about us",
+        "this_is_true": True,
+        "my_number" : 123,
+        "my_list" : [11,22,33,"ABC"],
+        "my_html": "<h1>Hello World</h1>"
+    }
+    return render(request, "about.html", my_context)
+
+def social_view(request,*args,**kwargs):
+    return HttpResponse("<h1>Social Page</h1>")
